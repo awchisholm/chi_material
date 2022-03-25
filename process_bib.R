e3608364efc100b44662b8047ebba86a0acc21ed
Andrew Chisholm <awchisholm@gmail.com>
2022-03-25 15:59:05 +0000

    save

--- process_bib.R
+++ process_bib.R
@@ -6,9 +6,10 @@ bibdf = bib2df::bib2df('Chichester.bib')
 #\item text from note \parencite{fewShowMeNumbers2012}.
 texdf =
   bibdf %>%
-  dplyr::select(BIBTEXKEY, NOTE) %>%
+  dplyr::select(BIBTEXKEY, NOTE, TITLE) %>%
   dplyr::mutate(NOTE = ifelse(is.na(NOTE), "", NOTE)) %>%
-  dplyr::mutate(item = paste0("\\item ", NOTE, " \\parencite{", BIBTEXKEY, "}.")) %>%
+  dplyr::mutate(TITLE = gsub(pattern='[{}]', replacement='', TITLE)) %>%
+  dplyr::mutate(item = paste0("\\item ", TITLE, '. ', NOTE, " \\parencite{", BIBTEXKEY, "}.")) %>%
   dplyr::select(item)
 
-
+writeLines(texdf$item, 'tex.txt')
