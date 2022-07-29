9f82d04fc51690ae985fbdf9669bca397d84015f
Andrew Chisholm <awchisholm@gmail.com>
2022-07-29 22:10:59 +0100

    save

--- process_bib.R
+++ process_bib.R
@@ -1,7 +1,10 @@
 #install.packages("bib2df")
 library(bib2df)
 library(dplyr)
-bibdf = bib2df::bib2df('Chichester.bib')
+if (file.exists('refs.qmd')) {
+  file.remove('refs.qmd')
+}
+bibdf = suppressMessages(bib2df::bib2df('Chichester.bib'))
 
 #\item text from note \parencite{fewShowMeNumbers2012}.
 texdf =
@@ -13,3 +16,12 @@ texdf =
   dplyr::select(item)
 
 writeLines(texdf$item, 'tex.txt')
+
+texdf2 <-
+  bibdf %>%
+  dplyr::select(BIBTEXKEY, NOTE, TITLE) %>%
+  dplyr::mutate(NOTE = ifelse(is.na(NOTE), "", NOTE)) %>%
+  dplyr::mutate(TITLE = gsub(pattern='[{}]', replacement='', TITLE)) %>%
+  dplyr::mutate(item = paste0('- ', TITLE, '. ',  NOTE, " [@", BIBTEXKEY, '].'))
+
+writeLines(texdf2$item, 'refs.qmd')
