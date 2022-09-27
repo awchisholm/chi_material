de36f5da98bdfac8387e6024682d76c29bd9e5df
Andrew Chisholm <awchisholm@gmail.com>
2022-09-27 11:59:24 +0100

    save

--- career.R
+++ career.R
@@ -1,11 +1,13 @@
 library(ggplot2)
 library(dplyr)
 career = read.csv('career.csv') %>% mutate(Opportunity = money, Year = year)
+career$Label[career$Label==''] = NA
 
 gg1 = ggplot(data=career, mapping = aes(x=Year, y=Opportunity))
 gg2 = ggplot(data=career, mapping = aes(x=Year, y=academia))
 ggp = geom_point()
 ggl = geom_line(size=3, col='darkblue')
-ggc = geom_col(mapping = aes(y=academia), fill='darkgreen', alpha=0.6)
+ggc = geom_col(mapping = aes(y=4*academia), fill='darkgreen', alpha=0.6)
 gglab =  theme(axis.text.y = element_blank())
-ggresult = gg1+ggl+ggc+gglab
+ggresult = gg1+ggl+ggc+gglab+ geom_label(mapping=aes(y=LabelPosition, label=Label)) + labs(y='Money')
+ggresult
