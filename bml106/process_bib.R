7aca2c8a857e02fe789a4472dcc5895f0325e1fb
Andrew Chisholm <awchisholm@gmail.com>
2023-02-22 15:12:25 +0000

    save

--- bml106/process_bib.R
+++ bml106/process_bib.R
@@ -1,26 +1,13 @@
 #install.packages("bib2df")
 library(bib2df)
 library(dplyr)
-if (file.exists('bml106/refs.qmd')) {
-  file.remove('bml106/refs.qmd')
+if (file.exists('refs.qmd')) {
+  file.remove('refs.qmd')
 }
 #bib_file = params$bibliography_file
 
-bibdf = suppressMessages(bib2df::bib2df('bml106/Chi106.bib'))
+bibdf = suppressMessages(bib2df::bib2df('Chi106.bib'))
 #bibdf = suppressMessages(bib2df::bib2df(bib_file))
-
-#\item text from note \parencite{fewShowMeNumbers2012}.
-#texdf =
-#  bibdf %>%
-#  dplyr::select(BIBTEXKEY, NOTE, TITLE) %>%
-#  dplyr::mutate(NOTE = ifelse(is.na(NOTE), "", NOTE)) %>%
-#  dplyr::mutate(TITLE = gsub(pattern='[{}]', replacement='', TITLE)) %>%
-#  dplyr::mutate(item = paste0("\\item ", TITLE, '. ', NOTE, " \\parencite{", BIBTEXKEY, "}.")) %>%
-#  dplyr::select(item)
-
-# original version - probably not needed any more
-# It used \item and \parencite - Quarto copes with -
-#writeLines(texdf$item, 'tex.txt')
 
 # This creates one bullet entry for each item in the bibliography file
 # It uses any notes found to add as text before the in-text citation
@@ -32,4 +19,4 @@ texdf2 <-
   dplyr::mutate(TITLE = gsub(pattern='[{}]', replacement='', TITLE)) %>%
   dplyr::mutate(item = paste0('- ', TITLE, '. ',  NOTE, " [@", BIBTEXKEY, '].'))
 
-writeLines(texdf2$item, 'bml106/refs.qmd')
+writeLines(texdf2$item, 'refs.qmd')
